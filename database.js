89e8d7eb11207a862935d389e84772f9b0700f3c
smith orakpo <smithorakpo@gmail.com>
2021-01-20 10:56:43 +0100

    add posts data

--- database.js
+++ database.js
@@ -3,6 +3,7 @@ const faker = require('faker')
 const create_database = () => {
   const database = {
     users: [],
+    posts: [],
   }
 
   for (let i = 0; i < 10; i++) {
@@ -12,10 +13,18 @@ const create_database = () => {
       last_name: faker.name.lastName(),
       email: faker.internet.email(),
     })
+
+    database.posts.push({
+      id: i,
+      userId: i,
+      title: faker.name.title(),
+      desc: faker.lorem.words(30),
+    })
   }
 
   return {
     users: database.users,
+    posts: database.posts,
   }
 }
 
